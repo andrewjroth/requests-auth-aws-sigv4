88280591f75f364a6063c3f35ee3c1281e24b506
Andrew Roth <andrew.roth@sapns2.com>
2022-07-15 16:48:58 -0400

    Some code cleanup and increased logging from CLI

--- requests_auth_aws_sigv4/__init__.py
+++ requests_auth_aws_sigv4/__init__.py
@@ -1,72 +1,71 @@
 #!/bin/env python3
 from __future__ import print_function
-import os
-import datetime
+
 import hashlib
 import hmac
 import logging
-from collections import OrderedDict
+import os
+from datetime import datetime
+
 from requests import __version__ as requests_version
 from requests.auth import AuthBase
-from requests.compat import urlencode, quote, urlparse
+from requests.compat import urlparse
+from requests.models import PreparedRequest
 
-
-USE_BOTO3 = False
 try:
     import boto3
-    USE_BOTO3 = True
 except ImportError:
-    pass
+    boto3 = None
 
 __version__ = '0.7'
-
 
 log = logging.getLogger(__name__)
 
 
 def sign_msg(key, msg):
-    ''' Sign message using key '''
+    """ Sign message using key """
     return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
 
 
 class AWSSigV4(AuthBase):
-    
+
     def __init__(self, service, **kwargs):
-        ''' Create authentication mechanism
-        
+        """ Create authentication mechanism
+
         :param service: AWS Service identifier, for example `ec2`.  This is required.
         :param region:  AWS Region, for example `us-east-1`.  If not provided, it will be set using
             the environment variables `AWS_DEFAULT_REGION` or using boto3, if available.
-        :param session: If boto3 is available, will attempt to get credentials using boto3, 
-            unless passed explicitly.  If using boto3, the provided session will be used or a new 
+        :param session: If boto3 is available, will attempt to get credentials using boto3,
+            unless passed explicitly.  If using boto3, the provided session will be used or a new
             session will be created.
-        
-        '''
+
+        """
         # Set Service
         self.service = service
-        if USE_BOTO3:
-            # Setup Session
-            if 'session' in kwargs:
-                if type(kwargs['session']) == boto3.Session:
-                    session = kwargs['session']
-                else:
-                    raise ValueError("Session must be boto3.Session, {} invalid, ".format(type(kwargs['session'])))
-            else:
-                session = boto3.Session()
-            log.debug("Using boto3 session: %s", session)
-        
+
         # First, get credentials passed explicitly
         self.aws_access_key_id = kwargs.get('aws_access_key_id')
         self.aws_secret_access_key = kwargs.get('aws_secret_access_key')
         self.aws_session_token = kwargs.get('aws_session_token')
         # Next, try environment variables or use boto3
         if self.aws_access_key_id is None or self.aws_secret_access_key is None:
-            if USE_BOTO3:
+            if boto3 is not None:
+                # Setup Session
+                if 'session' in kwargs:
+                    if type(kwargs['session']) == boto3.Session:
+                        session = kwargs['session']
+                    else:
+                        raise ValueError("Session must be boto3.Session, {} invalid, ".format(type(kwargs['session'])))
+                else:
+                    session = boto3.Session()
+                log.debug("Using boto3 session: %s", session)
                 cred = session.get_credentials()
                 log.debug("Got credential from boto3 session")
                 self.aws_access_key_id = cred.access_key
                 self.aws_secret_access_key = cred.secret_key
                 self.aws_session_token = cred.token
+                self.region = session.region_name
+                log.debug("Got region from boto3 session")
             else:
                 log.debug("Checking environment for credentials")
                 self.aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -75,46 +74,41 @@ class AWSSigV4(AuthBase):
         # Last, fail if still not found
         if self.aws_access_key_id is None or self.aws_secret_access_key is None:
             raise KeyError("AWS Access Key ID and Secret Access Key are required")
-        
+
         # Get Region passed explicitly
         self.region = kwargs.get('region')
         # Next, try environment variables or use boto3
         if self.region is None:
-            if USE_BOTO3:
-                self.region = session.region_name
-                log.debug("Got region from boto3 session")
-            else:
-                log.debug("Checking environment for region")
-                self.region = os.environ.get('AWS_DEFAULT_REGION')
+            log.debug("Checking environment for region")
+            self.region = os.environ.get('AWS_DEFAULT_REGION')
         # Last, fail if not found
         if self.region is None:
             raise KeyError("Region is required")
-    
-    
-    def __call__(self, r):
-        ''' Called to add authentication information to request
-        
+
+    def __call__(self, r: PreparedRequest) -> PreparedRequest:
+        """ Called to add authentication information to request
+
         :param r: `requests.models.PreparedRequest` object to modify
-        
+
         :returns: `requests.models.PreparedRequest`, modified to add authentication
-        
-        '''
+
+        """
         # Create a date for headers and the credential string
-        t = datetime.datetime.utcnow()
+        t = datetime.utcnow()
         self.amzdate = t.strftime('%Y%m%dT%H%M%SZ')
         self.datestamp = t.strftime('%Y%m%d')
         log.debug("Starting authentication with amzdate=%s", self.amzdate)
 
         # Parse request to get URL parts
-        p = urlparse(r.url)
-        log.debug("Request URL: %s", p)
-        host = p.hostname
-        uri = p.path
-        if len(p.query) > 0:
-            qs = dict(map(lambda i: i.split('='), p.query.split('&')))
+        url_parts = urlparse(r.url)
+        log.debug("Request URL: %s", url_parts)
+        host = url_parts.hostname
+        uri = url_parts.path
+        if len(url_parts.query) > 0:
+            qs = dict(map(lambda i: i.split('='), url_parts.query.split('&')))
         else:
             qs = dict()
-        
+
         # Setup Headers
         # r.headers is type `requests.structures.CaseInsensitiveDict`
         if 'Host' not in r.headers:
@@ -123,19 +117,19 @@ class AWSSigV4(AuthBase):
             r.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8'
         if 'User-Agent' not in r.headers:
             r.headers['User-Agent'] = 'python-requests/{} auth-aws-sigv4/{}'.format(
-                            requests_version, __version__)
+                requests_version, __version__)
         r.headers['X-AMZ-Date'] = self.amzdate
         if self.aws_session_token is not None:
             r.headers['x-amz-security-token'] = self.aws_session_token
 
-        ## Task 1: Create Cononical Request
-        ## Ref: http://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+        # Task 1: Create Canonical Request
+        # Ref: http://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
         # Query string values must be URL-encoded (space=%20) and be sorted by name.
         canonical_querystring = "&".join(map(lambda p: "=".join(p), sorted(qs.items())))
-        
+
         # Create payload hash (hash of the request body content).
         if r.method == 'GET':
-            payload_hash = hashlib.sha256(('').encode('utf-8')).hexdigest()
+            payload_hash = hashlib.sha256(''.encode('utf-8')).hexdigest()
         else:
             if r.body:
                 if isinstance(r.body, bytes):
@@ -148,38 +142,37 @@ class AWSSigV4(AuthBase):
                 log.debug("Request Body is empty")
                 payload_hash = hashlib.sha256(b'').hexdigest()
         r.headers['x-amz-content-sha256'] = payload_hash
-        
+
         # Create the canonical headers and signed headers. Header names
         # must be trimmed and lowercase, and sorted in code point order from
         # low to high. Note that there is a trailing \n.
         headers_to_sign = sorted(filter(lambda h: h.startswith('x-amz-') or h == 'host',
-            map(lambda H: H.lower(), r.headers.keys())))
+                                        map(lambda h_key: h_key.lower(), r.headers.keys())))
         canonical_headers = ''.join(map(lambda h: ":".join((h, r.headers[h])) + '\n', headers_to_sign))
         signed_headers = ';'.join(headers_to_sign)
-        
+
         # Combine elements to create canonical request
-        canonical_request = '\n'.join([r.method, uri, canonical_querystring, 
-            canonical_headers, signed_headers, payload_hash])
+        canonical_request = '\n'.join([r.method, uri, canonical_querystring,
+                                       canonical_headers, signed_headers, payload_hash])
         log.debug("Canonical Request: '%s'", canonical_request)
-        
-        ## Task 2: Create string to sign
+
+        # Task 2: Create string to sign
         credential_scope = '/'.join([self.datestamp, self.region, self.service, 'aws4_request'])
-        string_to_sign = '\n'.join(['AWS4-HMAC-SHA256', self.amzdate, 
-            credential_scope, hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()])
+        string_to_sign = '\n'.join(['AWS4-HMAC-SHA256', self.amzdate,
+                                    credential_scope, hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()])
         log.debug("String-to-Sign: '%s'", string_to_sign)
-        
-        ## Task 3: Calculate Signature
-        kDate = sign_msg(('AWS4' + self.aws_secret_access_key).encode('utf-8'), self.datestamp)
-        kRegion = sign_msg(kDate, self.region)
-        kService = sign_msg(kRegion, self.service)
-        kSigning = sign_msg(kService, 'aws4_request')
-        signature = hmac.new(kSigning, string_to_sign.encode('utf-8'), hashlib.sha256).hexdigest()
+
+        # Task 3: Calculate Signature
+        k_date = sign_msg(('AWS4' + self.aws_secret_access_key).encode('utf-8'), self.datestamp)
+        k_region = sign_msg(k_date, self.region)
+        k_service = sign_msg(k_region, self.service)
+        k_signing = sign_msg(k_service, 'aws4_request')
+        signature = hmac.new(k_signing, string_to_sign.encode('utf-8'), hashlib.sha256).hexdigest()
         log.debug("Signature: %s", signature)
-        
-        ## Task 4: Add signing information to request
+
+        # Task 4: Add signing information to request
         r.headers['Authorization'] = "AWS4-HMAC-SHA256 Credential={}/{}, SignedHeaders={}, Signature={}".format(
             self.aws_access_key_id, credential_scope, signed_headers, signature)
-        log.debug("Returning Request: <PreparedRequest method=%s, url=%s, headers=%s, SignedHeaders=%s, Signature=%s", 
-            r.method, r.url, r.headers, signed_headers, signature)
+        log.debug("Returning Request: <PreparedRequest method=%s, url=%s, headers=%s, SignedHeaders=%s, Signature=%s",
+                  r.method, r.url, r.headers, signed_headers, signature)
         return r
-
--- requests_auth_aws_sigv4/__main__.py
+++ requests_auth_aws_sigv4/__main__.py
@@ -1,11 +1,13 @@
 #!/bin/env python3
 from __future__ import print_function
-import logging
-import sys
-import os.path
+
 import argparse
+import logging
+import os.path
 import re
+import sys
 from collections import OrderedDict
+
 import requests
 from requests.compat import urlparse
 
@@ -13,10 +15,10 @@ from . import AWSSigV4
 
 
 def parse_response_headers(resp):
-    ''' Parse response to yield formatted header lines '''
+    """ Parse response to yield formatted header lines """
     yield "HTTP/{} {} {}".format((resp.raw.version / 10.0), resp.status_code, resp.reason)
-    for name, value in OrderedDict(sorted(resp.headers.items(), key=lambda t: t[0])).items():
-        yield "{}: {}".format(name, value)
+    for n, v in OrderedDict(sorted(resp.headers.items(), key=lambda t: t[0])).items():
+        yield "{}: {}".format(n, v)
 
 
 if __name__ == '__main__':
@@ -24,46 +26,46 @@ if __name__ == '__main__':
         prog_name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
     else:
         prog_name = os.path.basename(sys.argv[0])
-    
+
     logging.basicConfig()
     log = logging.getLogger(prog_name)
-    
+
     cli = argparse.ArgumentParser(prog=prog_name,
-        description='Send a request with AWS Signature V4 added for authentication')
-    
+                                  description='Send a request with AWS Signature V4 added for authentication')
+
     # Match cURL options, if possible
     cli.add_argument('url', help='Request URL')
     cli.add_argument('-i', '--include', action='store_true',
-        help="Include protocol response headers in the output")
+                     help="Include protocol response headers in the output")
     cli.add_argument('-H', '--header', action='append',
-        help="Pass custom header(s) to server")
+                     help="Pass custom header(s) to server")
     cli.add_argument('-v', '--verbose', action='store_true',
-        help="Make the operation more talkative")
+                     help="Make the operation more talkative")
     cli.add_argument('-X', '--request', default="GET", metavar="<command>",
-        choices=["DELETE", "GET", "HEAD",  "OPTIONS", "PATCH", "POST", "PUT"],
-        help="Specify request command to use")
+                     choices=["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"],
+                     help="Specify request command to use")
     cli.add_argument('-d', '--data', action='append',
-        help="HTTP POST data; changes request command to 'POST'")
-    
+                     help="HTTP POST data; changes request command to 'POST'")
+
     # Additional, non-cURL options
     cli.add_argument('--debug', action='store_true', help="Enable debug output")
     cli.add_argument('--service', help="Name of service for AWS Signature")
     cli.add_argument('--region', help="AWS Region Name")
-    
+
     # Parse args and make request
     args = cli.parse_args()
     if args.debug:
         log.setLevel(logging.DEBUG)
-    m = re.search('([a-z0-9-]+)\.([a-z]{2}-[a-z]+-\d{1})\.', args.url)
+    m = re.search(r'([a-z\d-]+)\.([a-z]{2}-[a-z]+-\d)\.', args.url)
     if args.service is None:
         if m:
-            log.debug("Guessing service from url: %s", m.group(1))
+            log.info("Guessing service from url: %s", m.group(1))
             args.service = m.group(1)
         else:
             print("Couldn't determine service, option --service is needed")
             sys.exit(2)
     if args.region is None and m is not None:
-        log.debug("Guessing region from url: %s", m.group(2))
+        log.info("Guessing region from url: %s", m.group(2))
         args.region = m.group(2)
     if args.data:
         args.request = 'POST'
@@ -71,37 +73,37 @@ if __name__ == '__main__':
     else:
         post_data = None
     if args.header:
-        headers = dict(map(lambda h: map(lambda i:  i.strip(), h.split(':')), args.header))
+        headers = dict(map(lambda h: map(lambda i: i.strip(), h.split(':')), args.header))
     else:
         headers = None
-    log.debug("Request: %s %s (service=%s, region=%s)", 
-        args.request, args.url, args.service, args.region)
+    log.debug("Request: %s %s (service=%s, region=%s)",
+              args.request, args.url, args.service, args.region)
     r = requests.request(args.request, args.url, headers=headers, data=post_data,
-        auth=AWSSigV4(args.service, region=args.region))
-    
+                         auth=AWSSigV4(args.service, region=args.region))
+
     # Output response
     log.debug("Response: %s %s", r.status_code, r.reason)
-    if args.verbose: # Print to sys.stderr for Verbose output
+    if args.verbose:  # Print to sys.stderr for Verbose output
         request_url = urlparse(r.request.url)
         print("> {} {} HTTP/{}".format(r.request.method, request_url.path, (r.raw.version / 10.0)),
-            file=sys.stderr)
+              file=sys.stderr)
         req_headers = r.request.headers.copy()
-        if 'Host' in req_headers: # Make sure Host header is first, if it exists
+        if 'Host' in req_headers:  # Make sure Host header is first, if it exists
             print("> Host:", req_headers.pop('Host'), file=sys.stderr)
         for key, value in req_headers.items():
             print("> {}: {}".format(key, value), file=sys.stderr)
-        print(">", file=sys.stderr) # Blank line at end of request headers
+        print(">", file=sys.stderr)  # Blank line at end of request headers
         for resp_line in parse_response_headers(r):
-            print("<", resp_line, file=sys.stderr) # Each response header
+            print("<", resp_line, file=sys.stderr)  # Each response header
     if args.include:
         for resp_line in parse_response_headers(r):
             print(resp_line)
-        print() # Blank line to seperate headers from data
+        print()  # Blank line to separate headers from data
     try:
         json_data = r.json()
-        if len(json_data) <= 1: # If only one item, print it's value
+        if len(json_data) <= 1:  # If only one item, print its value
             print("".join(json_data.values()))
         else:
             print(json_data)
-    except:
+    except ValueError:
         print(r.text)
