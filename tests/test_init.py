4f3591d85c9f42aabfe26f4bb700ba8458245b6a
james <james@listingmirror.com>
2022-09-29 12:09:54 -0400

    Each path segment of the Canonical URI must be encoded twice.

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,3 +1,5 @@
+import urllib.parse
+
 import pytest
 from requests.models import Request
 from mockito import when, spy2, unstub
@@ -87,6 +89,29 @@ def test_call_simple(frozentime):
         f"AWS4-HMAC-SHA256 Credential=key_id/{frozentime:%Y%m%d}/test-region-1/test/aws4_request",
         f"SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token",
         f"Signature=817adf89563cdf0728f4e684c9ac9ead2cca25c4610e9bb3ddfebe8665ea72f2"
+    ])
+    assert result.headers['Host'] == "testhost"
+    assert result.headers['Content-Type'] == "application/x-www-form-urlencoded; charset=utf-8"
+    assert result.headers['User-Agent'] == 'python-requests/{} auth-aws-sigv4/{}'.format(
+        requests_auth_aws_sigv4.requests_version, requests_auth_aws_sigv4.__version__)
+    assert result.headers['X-AMZ-Date'] == frozentime.strftime('%Y%m%dT%H%M%SZ')
+    assert result.headers['x-amz-security-token'] == "token"
+    assert result.headers['x-amz-content-sha256'] == "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+
+def test_uri_double_encoded_segment(frozentime):
+    aws_auth = requests_auth_aws_sigv4.AWSSigV4('test', region='test-region-1',
+                                                aws_access_key_id="key_id",
+                                                aws_secret_access_key="secret_key",
+                                                aws_session_token="token")
+    segment = urllib.parse.quote('123 / abc', safe='')
+    req = Request('GET', f'https://testhost/action/{segment}?param=value')
+    result = aws_auth(req.prepare())
+    assert 'Authorization' in result.headers
+    assert result.headers['Authorization'] == ", ".join([
+        f"AWS4-HMAC-SHA256 Credential=key_id/{frozentime:%Y%m%d}/test-region-1/test/aws4_request",
+        f"SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token",
+        f"Signature=89a91408e96df9231d23bc51aa31916f1f72db68d6e96ba633c6652ba86fe2cd"
     ])
     assert result.headers['Host'] == "testhost"
     assert result.headers['Content-Type'] == "application/x-www-form-urlencoded; charset=utf-8"
